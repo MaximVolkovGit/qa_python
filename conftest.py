e9d96bd4e90190e3e0a45e8e91e6143f525a8bf4
Maxim Volkov <fateplayer@yandex.ru>
2025-08-26 11:12:07 +0300

    6 tests and fixtures

--- conftest.py
+++ conftest.py
@@ -3,5 +3,14 @@ from main import BooksCollector
 
 @pytest.fixture
 def collection():
-    collector = BooksCollector()
-    return collector
+    collection = BooksCollector()
+    return collection
+
+@pytest.fixture
+def test_library():
+    return {
+            'Джуманджи': 'Фантастика',
+            'Властелин Колец': 'Фантастика', 
+            'Молчание ягнят': 'Ужасы',
+            'Пуаро': 'Детективы',
+        }
